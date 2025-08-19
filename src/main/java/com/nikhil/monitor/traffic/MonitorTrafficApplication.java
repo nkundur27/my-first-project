ae3c2c55a34f88fbdd986ba23a9846336d0846b2
kundur nikhil <nikhilkunduur9@gmail.com>
2025-08-19 15:43:15 +0530

    adding files

---
+++ src/main/java/com/nikhil/monitor/traffic/MonitorTrafficApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.monitor.traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorTrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorTrafficApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikhil/monitor/traffic/MonitorTrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.monitor.traffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorTrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
